7cceda79e76237cc213969123d077f5b289a6539
Samwise <samuelfoster0@gmail.com>
2020-03-25 19:56:01 -0700

    Fixed the merge error that would delete the css dropdown line.

--- assets/js/landing-page-script.js
+++ assets/js/landing-page-script.js
@@ -1,14 +1,6 @@
 $(document).ready(function(){
-<<<<<<< HEAD
     $(".dropdown-trigger").dropdown({
         coverTrigger:false
     });
-});
-
-
-
-=======
-    $(".dropdown-trigger").dropdown();
     $(".sidenav").sidenav();
 });
->>>>>>> 6ad13f0b093e4f359d394f63b1df8f32992b77f7
